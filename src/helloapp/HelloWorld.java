2ab4b3967eab92b28aa5a9897cf52947c6cf3516
Phillip Dudley <Phillip.Dudley@cerner.com>
2015-05-03 21:00:23 -0500

    Initial Commit

---
+++ src/helloapp/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+* Continuously prints Hello World for testing purposes
+*
+*/
+
+package helloapp;
+
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		while( true )
+		{
+			System.out.println("Hello WOrld");
+		}
+	}
+}
